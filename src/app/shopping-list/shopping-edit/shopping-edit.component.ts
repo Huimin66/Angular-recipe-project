92ae982c0d3decf6af2a7aeac0874c6a991c05e4
huimin <huim.cao@gmail.com>
2022-09-08 00:20:22 +0200

    Finish chapter 6

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../../schared/ingredient.model';
+import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInputRef: ElementRef = {} as ElementRef
+  @ViewChild('amountInput') amountInputRef: ElementRef = {} as ElementRef
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(){
+    const ingredientName = this.nameInputRef.nativeElement.value
+    const ingredientAmount = this.amountInputRef.nativeElement.value
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount)
+    this.ingredientAdded.emit(newIngredient)
+    
+  }
 }
